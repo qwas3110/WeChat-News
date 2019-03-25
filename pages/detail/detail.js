624e982e3576f73059301fd07e7b2608ae05711f
qwas3110 <283994917@qq.com>
2019-03-25 16:16:06 +0800

    动态获取数据，点击事件-新闻详情页面

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,1 +1,47 @@
-Page({})
+
+Page({
+	data: {
+		id: '',
+		newsDetail: {},
+		newsContent: []
+		
+	},
+	onLoad(options) {
+		this.setData({ id: options.id });
+		this.getNewsDetail();
+		
+	},
+	getNewsDetail() {
+		wx.request({
+			url: 'https://test-miniprogram.com/api/news/detail',
+			data: {
+				id: this.data.id
+			},
+
+			success: (res) => {
+				const result = res.data.result;
+				console.log(result);
+				this.setBasicInfomation(result);
+				this.setNewsBasicText(result);
+			}
+		})
+	},
+	// 新闻详情基本信息
+	setBasicInfomation(result) {
+		let newsDetail = {};
+		newsDetail.title = result.title,
+		newsDetail.source = result.source,
+		newsDetail.id = result.id,
+		newsDetail.readCount = result.readCount,
+		newsDetail.firstImage = result.firstImage,
+		newsDetail.date = result.date.slice(0, 9)
+		
+		this.setData({newsDetail:newsDetail})
+	},
+	// 新闻详细文本
+	setNewsBasicText(result) {
+		let content = result.content;
+		let newsContent = content;
+		this.setData({newsContent:newsContent});
+	}
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,58 +1,106 @@
+const stringMap = {
+	'国内': 'gn',
+	'国际': 'gj',
+	'财经': 'cj',
+	'娱乐': 'yl',
+	'军事': 'js',
+	'体育': 'ty',
+	'其他': 'other'
+}
+
+
 Page({
   data: {
     hot: {},
-		newsList: []
+    newsList: [],
+    type: 'gn',
+    typeMap: {
+      'gn': '国内',
+      'gj': '国际',
+      'cj': '财经',
+      'yl': '娱乐',
+      'js': '军事',
+      'ty': '体育',
+      'other': '其他'
+    }
   },
+	// 下拉刷新
+	onPullDownRefresh() {
+		this.getNewsAPI(() => wx.stopPullDownRefresh());
+	},
+
   onLoad() {
     this.getNewsAPI();
   },
   // 调用新闻API
-  getNewsAPI() {
+  getNewsAPI(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: {
-        type: 'gn'
+        type: this.data.type
       },
 
       success: (res) => {
-        console.log(res);
         const result = res.data.result;
-        const hotNews = result[8];
+				console.log(result);
+        const hotNews = result[0];
         // 调用数据
         this.setHotNews(hotNews);
-				this.setNewsList(result);
-      }
+        this.setNewsList(result);
+      },
+
+			complete: () => {
+				callback && callback();
+			}
     })
   },
   // 将调用数据渲染到热门页面
   setHotNews(result) {
     let hot = {};
-    hot.title = result.title;
+    hot.title = result.title || '未知来源';
     hot.date = result.date.slice(0, 10),
-    hot.time = result.date.slice(14, 19),
-    hot.source = result.source,
-    hot.imgSrc = `https:${result.firstImage}`,
-    hot.id = result.id
-    console.log(hot);
+      hot.time = result.date.slice(14, 19),
+      hot.source = result.source,
+      hot.imgSrc = `${result.firstImage}`,
+      hot.id = result.id
+
     this.setData({
       hot: hot
     })
   },
-	// 将调用数据渲染到新闻列表
-	setNewsList(result) {
-		let newsList = [];
-		// 遍历数据
-		for (let x = 0; x < result.length; x++) {
-			newsList.push({
-				id: result[x].id,
-				title: result[x].title,
-				time: result[x].date.slice(14,19),
-				source: result[x].source,
-				imgSrc: `https:${result[x].firstImage}`
-			})
-		}
-		this.setData({
-			newsList: newsList
+  // 将调用数据渲染到新闻列表
+  setNewsList(result) {
+    let newsList = [];
+    // 遍历数据
+    for (let x = 0; x < result.length; x++) {
+      newsList.push({
+        id: result[x].id,
+        title: result[x].title || '未知来源',
+        time: result[x].date.slice(14, 19),
+        source: result[x].source,
+        imgSrc: `${result[x].firstImage}`
+      })
+    }
+    this.setData({
+      newsList: newsList
+    })
+  },
+  // event.target.dataset.type'
+	// 切换不同新闻类型
+	onTapNews(event) {
+		let type = stringMap[event.target.dataset.type];
+		this.setData({type:type})
+		this.getNewsAPI();
+	},
+	// 切换至新闻详情页面
+	onTapNewsDetail(event) {
+;
+		let id = event.currentTarget.dataset.id;
+		wx.navigateTo({
+			url: `/pages/detail/detail?id=${id}`,
+			success: function(res) {},
+			fail: function(res) {},
+			complete: function(res) {},
 		})
 	}
 })
